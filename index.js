ab363978a3c9245e1dba90b6bf319bcba96075eb
Simplysaad <148543136+Simplysaad@users.noreply.github.com>
2024-10-29 16:20:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const ejs = require("ejs");
+//const expressFileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+dotenv.config();
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const mongoStore = require("connect-mongo");
+const connectDb = require("./Server/Config/db.js");
 
+//START APP
+const app = express();
+let PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`app is listening on http://localhost:${PORT}`);
+});
 
-const express = require('express')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const cookieParser = require('cookie-parser')
-const connectDb = require('./server/Config/db.js')
-const expressLayouts = require("express-ejs-layouts")
-require('dotenv').config()
+//CONNECT DATABASE
+connectDb();
 
-const app = express()
-const PORT = process.env.PORT || 8080
-app.listen(PORT, ()=>{
-  console.log(`app listening at http://localhost:${PORT}`)
-})
-connectDb()
+//app.use(expressFileUpload());
 
-//body parser to json
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+//SESSIONS INITIALIZING
+app.use(
+    session({
+        secret: process.env.SECRET_KEY,
+        store: mongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        }),
+        saveUninitialized: false,
+        resave: false,
+        cookies: {
+            maxAge: 3600000,
+            secure: false
+        }
+    })
+);
 
-//EXPRESS SESSIONS
-app.use(session({
-  store: MongoStore.create({
-    mongoUrl : process.env.MONGO_URI
-  }),
-  secret: process.env.SECRET_KEY,
-  resave: false,
-  saveUninitialized: true,
-  cookies:{
-    maxAge: 3600000,
-    secure: false
-  }
-}))
+//VIEW ENGINE
+app.set("view engine", "ejs");
+app.set("views", "Views");
 
-//USE COOKIE PARSER 
-app.use(cookieParser())
+//LAYOUT
+app.use(expressLayout);
+app.set("layout", "Layouts/main");
 
-//VIEWS
-app.set("view engine", "ejs")
-app.set("views", "views")
+//LOAD STATIC FILES
+app.use(express.static("Public"));
 
-//LAYOUTS
-app.use(expressLayouts)
-app.set("layout", "layouts/main")
-
-//STATIC
-app.use(express.static('PUBLIC'))
+//PARSE req.body TO JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //ROUTING
-app.use("/", require('./server/Routes/admin_routes.js'))
-app.use("/", require('./server/Routes/routes.js'))
+app.use("/admin", require("./server/Routes/admin.js"));
+app.use("/vendor", require("./server/Routes/vendor.js"));
+app.use("/", require("./server/Routes/routes.js"));
 
