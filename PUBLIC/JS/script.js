3024e6995807ceb0917654df683a32786ef2b791
Simplysaad <148543136+Simplysaad@users.noreply.github.com>
2024-11-07 06:35:28 +0100

    update PUBLIC/JS/script.js

--- PUBLIC/JS/script.js
+++ PUBLIC/JS/script.js
@@ -1,25 +1,102 @@
-const form = document.querySelector("#form");
-const btnRegister = document.querySelector("#btnRegister");
-const emailAddress = document.querySelector("#emailAddress");
+const charArr = ["a", "b", "c", "d", "e", "f", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const ratings = [5, 4, 5, 5, 3, 1, 5, 3, 5, 5, 3, 4,]
 
-let emailError = document.querySelector(".email-error");
 
-emailAddress.addEventListener("change", async e => {
-    e.preventDefault();
+function getRandomColor() {
+  let color = "#"
+  for (let i = 0; i < 6; i++) {
+    let ranInt = Math.floor(Math.random() * 16)
+    color += charArr[ranInt]
+  }
+  return color
+}
+let cardImage = document.querySelectorAll(".card-image")
+let previewImage = document.querySelectorAll(".preview-image")
+randomImageColor(cardImage)
+randomImageColor(previewImage)
+function randomImageColor(Images) {
+  Images.forEach((image)=> {
+    image.textContent += getRandomColor()
+    image.style.backgroundColor = getRandomColor()
+  })
+}
 
-    try {
-        const response = await fetch("/validate", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ emailAddress: emailAddress })
-        });
-        const responseData = await response.json();
-        console.log(responseData);
 
-        emailError.innerHTML = responseData.message;
-    } catch (err) {
-        console.error(err);
+const btnSearch = document.querySelector("#btnSearch")
+const searchContainer = document.querySelector("#searchContainer")
+const searchInput = document.querySelector("#searchInput")
+
+const showSearchCont = ()=> {
+  let searchDisplay = searchContainer.style.visibility
+
+  if (searchDisplay === "visible") {
+    searchContainer.style.visibility = "hidden"
+  } else {
+    searchContainer.style.visibility = "visible"
+    searchInput.focus()
+  }
+}
+
+searchInput.addEventListener("blur", showSearchCont)
+btnSearch.addEventListener("click", showSearchCont)
+
+
+
+
+
+function getAverage(arr) {
+  let sum = 0;
+  let average;
+  for (i = 0; i < arr.length; i++) {
+    if (typeof(arr[i]) === "number") {
+      sum += arr[i]
     }
-});
+  }
+  average = sum/arr.length
+  return average
+}
+function createElement(parentId, tag, classes, textContent, src) {
+  let element = document.createElement(tag)
+  let parent = document.getElementById(parentId)
+  element.textContent = textContent
+
+  if (typeof(classes) === "object") {
+    classes.forEach((oneclass) => {
+      element.classList.add(oneclass)
+    })
+  } else {
+    element.classList.add(classes)
+  }
+  if (tag === "img") {
+    element.src = src
+  }
+  element.href = src
+  parent.append(element)
+}
+
+function ratingStars() {
+  let avgRating = Math.ceil(getAverage(ratings))
+
+  for (i = 0; i < avgRating; i++) {
+    createElement("ratingStars", "span", ["fa", "fa-star"])
+  }
+  for (i = 0; i < (5-avgRating); i++) {
+    createElement("ratingStars", "span", ["fa-regular", "fa-star"])
+  }
+}
+ratingStars()
+
+const btnCart = document.getElementById('btnCart')
+const threshold = btnCart.offsetTop
+//console.log(threshold)
+window.addEventListener("scroll", ()=> {
+  if (window.scrollY >= threshold) {
+    btnCart.classList.add('sticky')
+    //console.log("ive passed by "+ window.scrollY)
+  } else {
+    btnCart.classList.remove('sticky')
+  }
+})
+if (window.width >= 600) {
+  btnCart.classList.remove('sticky')
+}
