d1e8172cacd1713621dd471f06cb241320c66d4b
Simplysaad <148543136+Simplysaad@users.noreply.github.com>
2024-11-12 00:31:27 +0100

    update server/Routes/vendorRoutes.js

---
+++ server/Routes/vendorRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: "./Public/Uploads/",
+    filename: function (req, file, cb) {
+        let myFileName =
+            Math.round(Math.random() * 100372600) +
+            "_" +
+            file.originalname +
+            "_" +
+            Date.now() +
+            ".jpg";
+        cb(null, myFileName);
+    }
+});
+const upload = multer({
+    storage: storage
+});
+
+const Product = require("../Models/Product.js");
+const User = require("../Models/User.js");
+const Order = require("../Models/Order.js");
+
+const helper = require("../.././utils/helper.js");
+const relatedProductsFunc = helper.relatedProductsFunc;
+
+const locals = {
+    title: "Campus Mart",
+    description:
+        "an incampus shopping website for school online vendors and students, to buy , sell and deliver items without hassle",
+    imageUrl: "/IMG/favicon.png"
+};
+
