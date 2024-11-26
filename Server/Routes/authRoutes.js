45b855dec34ec16d7c65777f8f33a54222a5fe13
Saad Idris <148543136+Simplysaad@users.noreply.github.com>
2024-11-26 10:32:49 +0100

    update Server/Routes/authRoutes.js

--- Server/Routes/authRoutes.js
+++ Server/Routes/authRoutes.js
@@ -51,11 +51,12 @@ router.post("/register", async (req, res) => {
 
 router.get("/login", async (req, res) => {
     try {
-      if(req.session && req.session.userId){
-        req.flash("info", "user logged out successfully")
-        res.redirect("/auth/logout")
-      }
-        res.render("Auth/login");
+        if (req.session && req.session.userId) {
+            req.flash("info", "user logged out successfully");
+            res.redirect("/auth/logout");
+        } else {
+            res.render("Auth/login");
+        }
     } catch (err) {
         console.error(err);
     }
