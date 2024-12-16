0bfebfbc48324b3cfe8d6ba85b565df60cec675a
Saad Idris <148543136+Simplysaad@users.noreply.github.com>
2024-12-16 08:19:19 +0100

    update PUBLIC/JS/authScript.js

--- PUBLIC/JS/authScript.js
+++ PUBLIC/JS/authScript.js
@@ -99,7 +99,7 @@ const validateRegister = async () => {
         const response = await fetch("/auth/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ emailAddress, password }) // Include password in request body
+            body: JSON.stringify({ emailAddress, password }) 
         });
 
         const data = await response.json();
