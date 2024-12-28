14eadfe3a6dc6c92deab3de68f51afe450045b8e
Saad Idris <148543136+Simplysaad@users.noreply.github.com>
2024-12-28 08:59:58 +0100

    update PUBLIC/JS/register.js

--- PUBLIC/JS/register.js
+++ PUBLIC/JS/register.js
@@ -1,87 +1,86 @@
 const roleSelector = document.getElementById("roleSelector");
-const personalDetails = document.getElementById("personalDetails");
 const businessDetails = document.getElementById("businessDetails");
-const roles = document.getElementsByName("role");
+//const personalDetails = document.getElementById("personalDetails");
 
-const showBusinessBtn = document.querySelector("#showBusinessBtn");
 const showBusinessDiv = document.querySelector("#showBusinessDiv");
-const registerForm = document.querySelector("#registerForm");
 
-const preventDefault = e => {
-    e.preventDefault();
-};
+const inputPassword = document.getElementById("password");
+const inputEmail = document.getElementById("emailAddress");
+const inputUsername = document.getElementById("username");
+const confirmPassword = document.getElementById("confirmPassword");
+const checkAgreement = document.getElementById("checkAgreement");
+
+const errorMessage = document.querySelector("#errorMessage");
+
+const roles = document.querySelectorAll(".checkbox");
+
 roles.forEach(role => {
-    role.addEventListener("click", () => {
-        businessDetails.style.display = "none";
-        if (role.value === "vendor") {
+    businessDetails.style.display = "none";
+    role.addEventListener("input", () => {
+        console.log("role", role.value);
+        if (role.value !== "customer") {
             businessDetails.style.display = "block";
-            showBusinessBtn.addEventListener("click", preventDefaultListener);
             showBusinessDiv.style.display = "none";
-        } else if (role.value === "customer") {
+        } else {
             businessDetails.style.display = "none";
             showBusinessDiv.style.display = "block";
-            showBusinessBtn.removeEventListener(
-                "click",
-                preventDefaultListener
-            );
         }
     });
 });
 
-const showPasswordRegister = () => {
-    const inputPassword = document.getElementById("password");
-    const loginPassword = document.getElementById("loginPassword");
-    const confirmPassword = document.getElementById("confirmPassword");
-    console.log(inputPassword.type);
-
-    const isVisible =
-        inputPassword.type === "text" || loginPassword.type === "text";
-
+const showPassword_register = () => {
+    const isVisible = inputPassword.type === "text";
     inputPassword.type = isVisible ? "password" : "text";
-    loginPassword.type = isVisible ? "password" : "text";
     confirmPassword.type = isVisible ? "password" : "text";
 };
 
-const btnShowPassword = document.querySelectorAll(".btnShowPassword");
-btnShowPassword.forEach(btn => {
-    btn.addEventListener("click", e => {
-        e.preventDefault();
-        showPasswordRegister();
-    });
+const btnShowPassword = document.querySelector(".btnShowPassword");
+btnShowPassword.addEventListener("click", e => {
+    e.preventDefault();
+    showPassword_register();
 });
 
-const validateRegister = async () => {
-    const emailAddress = document.querySelector("#emailAddress").value;
-    const password = document.querySelector("#password").value;
-    const errorMessage = document.querySelector("#errorMessage");
+const registerForm = document.querySelector("#registerForm");
+registerForm.addEventListener("submit", async e => {
+    e.preventDefault();
+
+    const emailAddress = inputEmail.value;
+    const password = inputPassword.value;
+    const username = inputUsername.value;
+    const confirmPasswordValue = confirmPassword.value;
 
     try {
-        //const response = await
-        const response = await fetch("/auth/validate-register", {
+        if (password !== confirmPasswordValue) {
+            errorMessage.textContent = "Passwords do not match";
+            confirmPassword.focus();
+            confirmPassword.classList.add("border-danger");
+            
+            return false;
+        } else if (!checkAgreement.checked) {
+            errorMessage.textContent = "Agree to Terms and conditions ";
+            checkAgreement.focus();
+            return false;
+        }
+        const response = await fetch("/auth/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ emailAddress, password })
+            body: JSON.stringify({ emailAddress, username, password })
         });
+        console.log(JSON.stringify(response));
         const data = await response.json();
-        if (!data.success) {
-            console.log("data", data);
-            errorMessage.textContent =
-                data.errorMessage || "Invalid credentials";
+
+        if (data.success) {
+            window.location.href = data.redirect;
+            return;
+        } else {
+            errorMessage.textContent = data.errorMessage;
+            inputEmail.focus();
+            inputEmail.classList.add("border-danger");
+
             return false;
         }
     } catch (error) {
         console.error(error);
         return false;
-    }
-};
-
-const btnSubmitRegister = document.querySelector("#btnSubmitRegister");
-
-btnSubmitRegister.addEventListener("click", async e => {
-    e.preventDefault();
-    const isValid = await validateRegister();
-
-    if (isValid) {
-        registerForm.submit();
     }
 });
