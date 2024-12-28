8d791254cb4edd57d269de2883dfde9c03e9d673
Saad Idris <148543136+Simplysaad@users.noreply.github.com>
2024-12-28 09:00:25 +0100

    update PUBLIC/JS/login.js

--- PUBLIC/JS/login.js
+++ PUBLIC/JS/login.js
@@ -1,42 +1,37 @@
 const loginForm = document.querySelector("#loginForm");
 
-const username = document.querySelector("#username").value;
-const password = document.querySelector("#loginPassword").value;
+const usernameElement = document.querySelector("#username");
+const passwordElement = document.querySelector("#loginPassword");
 const errorMessage = document.querySelector("#errorMessage");
-
-const validateLogin = async () => {
-    try {
-        const response = await fetch("/auth/validate-login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username, password })
-        });
-        const data = await response.json();
-        if (!data.success) {
-            console.log("data", data);
-            errorMessage.textContent =
-                data.errorMessage || "Invalid credentials";
-            return false;
-        } else return true;
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
-};
-
 const btnSubmitLogin = document.getElementById("btnSubmitLogin");
 
-btnSubmitLogin.addEventListener("click", async e => {
-    e.preventDefault();
-    let isValid = await validateLogin();
-
-    if (isValid) {
-        loginForm.submit();
-        return true;
-    } else {
-        console.log("Login attempt invalid");
-        errorMessage.textContent = "invalid username or password";
-        return false;
+loginForm.addEventListener("submit", e => {
+    try {
+        e.preventDefault();
+        fetch("/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                username: usernameElement.value,
+                password: passwordElement.value
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (!data.success) {
+                    console.log("data", data.errorMessage);
+                    errorMessage.textContent =
+                        data.errorMessage || "Invalid credentials";
+                    return false;
+                } else {
+                    window.location.href = data.redirect;
+                    return;
+                }
+            })
+            .catch(err => console.error(err));
+    } catch (error) {
+        console.error(error);
+        return;
     }
 });
 
