d969a8141a5fbae7ab3b4cb4d96aa97483db9392
Saad Idris <148543136+Simplysaad@users.noreply.github.com>
2025-02-10 15:03:37 +0100

    update PUBLIC/JS/script.js

--- PUBLIC/JS/script.js
+++ PUBLIC/JS/script.js
@@ -1,64 +1,4 @@
 
-// Array of characters used for generating random colors
-let charArray = ["a", "b", "c", "d", "e", "f", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-// Array of ratings, this should come from database
-const ratings = [5, 4, 5, 5, 3, 1, 5, 3, 5, 5, 3, 4];
-
-
-
-/**
- * getRatings()
- * Fetches ratings data from the server.
- * @returns {Promise<Response>} A promise that resolves to the fetched ratings data.
- */
-const getRatings = async () => {
-    try {
-        const ratings = await fetch("/getRating", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-         return ratings;
-    } catch (err) {
-        console.error("Error fetching ratings:", err);
-        return null; // Or handle error appropriately
-    }
-};
-
-/**
- * getRandomColor()
- * Generates a random hexadecimal color code.
- * @returns {string} A random color code in the format #RRGGBB.
- */
-function getRandomColor() {
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        let ranInt = Math.floor(Math.random() * 16);
-        color += charArray[ranInt];
-    }
-    return color;
-}
-
-/**
- * randomImageColor()
- * Applies a random background color to each image element in the provided NodeList.
- * @param {NodeList} Images - A NodeList of HTML elements representing images.
- */
-function randomImageColor(Images) {
-    Images.forEach(image => {
-        image.style.backgroundColor = getRandomColor();
-    });
-}
-// Select all card and preview images
-let cardImage = document.querySelectorAll(".card-image");
-let previewImage = document.querySelectorAll(".preview-image");
-// Apply random colors to card and preview images
-randomImageColor(cardImage);
-randomImageColor(previewImage);
-
-
-
 /**
  * toggleDisplay()
  * Function to toggle display of html element
@@ -88,6 +28,96 @@ toggleDisplay(searchCont, btnShowSearch)
 
 
 
+const anchorTags = document.querySelectorAll("a[href]")
+anchorTags.forEach(tag => {
+  tag.classList.toggle("active", tag.href == window.location.href)
+  // if(tag.href == window.location.href)
+  //   tag.classList.add("active")
+  //   else
+  //   tag.classList.remove("active")
+})
+
+
+// Array of characters used for generating random colors
+let charArray = ["a", "b", "c", "d", "e", "f", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// Array of ratings, this should come from database
+const ratings = [5, 4, 5, 5, 3, 1, 5, 3, 5, 5, 3, 4];
+
+
+/**
+ * ratingStars()
+ * Creates star elements based on the average rating and appends them to the specified container.
+ */
+function ratingStars() {
+  let avgRating = Math.ceil(getAverage(ratings));
+
+  for (let i = 0; i < avgRating; i++) {
+    createElement("ratingStars", "span", ["fa", "fa-star"]);
+  }
+  for (let i = 0; i < 5 - avgRating; i++) {
+    createElement("ratingStars", "span", ["fa-regular", "fa-star"]);
+  }
+}
+// Create rating stars on page load
+ratingStars();
+
+/**
+ * getRatings()
+ * Fetches ratings data from the server.
+ * @returns {Promise<Response>} A promise that resolves to the fetched ratings data.
+ */
+const getRatings = async () => {
+  try {
+    const ratings = await fetch("/getRating", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    return ratings;
+  } catch (err) {
+    console.error("Error fetching ratings:", err);
+    return null; // Or handle error appropriately
+  }
+};
+
+/**
+ * getRandomColor()
+ * Generates a random hexadecimal color code.
+ * @returns {string} A random color code in the format #RRGGBB.
+ */
+function getRandomColor() {
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    let ranInt = Math.floor(Math.random() * 16);
+    color += charArray[ranInt];
+  }
+  return color;
+}
+
+/**
+ * randomImageColor()
+ * Applies a random background color to each image element in the provided NodeList.
+ * @param {NodeList} Images - A NodeList of HTML elements representing images.
+ */
+function randomImageColor(Images) {
+  Images.forEach(image => {
+    image.style.backgroundColor = getRandomColor();
+  });
+}
+// Select all card and preview images
+let cardImage = document.querySelectorAll(".card-image");
+let categoryBox = document.querySelectorAll(".category-box");
+let previewImage = document.querySelectorAll(".preview-image");
+let randomColorImage = document.querySelectorAll(".random-color");
+// Apply random colors to card and preview images
+randomImageColor(cardImage);
+randomImageColor(categoryBox);
+randomImageColor(randomColorImage);
+randomImageColor(previewImage);
+
+
+
 // Select search elements
 const btnSearch = document.querySelector("#btnSearch");
 const searchContainer = document.querySelector("#searchContainer");
@@ -98,14 +128,14 @@ const searchInput = document.querySelector("#searchInput");
  * Toggles the visibility of the search container.
  */
 const showSearchCont = () => {
-    let searchDisplay = searchContainer.style.visibility;
+  let searchDisplay = searchContainer.style.visibility;
 
-    if (searchDisplay === "visible") {
-        searchContainer.style.visibility = "hidden";
-    } else {
-        searchContainer.style.visibility = "visible";
-        searchInput.focus();
-    }
+  if (searchDisplay === "visible") {
+    searchContainer.style.visibility = "hidden";
+  } else {
+    searchContainer.style.visibility = "visible";
+    searchInput.focus();
+  }
 };
 // Add event listeners to toggle search container
 searchContainer.addEventListener("blur", showSearchCont);
@@ -118,15 +148,15 @@ btnSearch.addEventListener("click", showSearchCont);
  * @returns {number} The average of the numeric values in the array.
  */
 function getAverage(arr) {
-    let sum = 0;
-    let average;
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === "number") {
-            sum += arr[i];
-        }
+  let sum = 0;
+  let average;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      sum += arr[i];
     }
-    average = sum / arr.length;
-    return average;
+  }
+  average = sum / arr.length;
+  return average;
 }
 
 /**
@@ -139,40 +169,24 @@ function getAverage(arr) {
  * @param {string} src - The source URL for the new element if its a img or href for anchor element
  */
 function createElement(parentId, tag, classes, textContent, src) {
-    let element = document.createElement(tag);
-    let parent = document.getElementById(parentId);
-    element.textContent = textContent;
+  let element = document.createElement(tag);
+  let parent = document.getElementById(parentId);
+  element.textContent = textContent;
 
-    if (Array.isArray(classes)) {
-        classes.forEach(oneclass => {
-            element.classList.add(oneclass);
-        });
-    } else {
-        element.classList.add(classes);
-    }
-    if (tag === "img") {
-        element.src = src;
-    }
-    element.href = src;
-    parent.append(element);
+  if (Array.isArray(classes)) {
+    classes.forEach(oneclass => {
+      element.classList.add(oneclass);
+    });
+  } else {
+    element.classList.add(classes);
+  }
+  if (tag === "img") {
+    element.src = src;
+  }
+  element.href = src;
+  parent.append(element);
 }
 
-/**
- * ratingStars()
- * Creates star elements based on the average rating and appends them to the specified container.
- */
-function ratingStars() {
-    let avgRating = Math.ceil(getAverage(ratings));
-
-    for (let i = 0; i < avgRating; i++) {
-        createElement("ratingStars", "span", ["fa", "fa-star"]);
-    }
-    for (let i = 0; i < 5 - avgRating; i++) {
-        createElement("ratingStars", "span", ["fa-regular", "fa-star"]);
-    }
-}
-// Create rating stars on page load
-ratingStars();
 
 // Select the cart button
 const btnCart = document.getElementById("btnCart");
@@ -184,15 +198,15 @@ const threshold = btnCart.offsetTop;
  * based on user scroll Y position
  */
 window.addEventListener("scroll", () => {
-    if (window.scrollY >= threshold) {
-        btnCart.classList.add("sticky");
-    } else {
-        btnCart.classList.remove("sticky");
-    }
+  if (window.scrollY >= threshold) {
+    btnCart.classList.add("sticky");
+  } else {
+    btnCart.classList.remove("sticky");
+  }
 });
 // Remove sticky from the cart if widnow size is greater than 600px
 if (window.width >= 600) {
-    btnCart.classList.remove("sticky");
+  btnCart.classList.remove("sticky");
 }
 // Select all tag button elements
 const btnTag = document.querySelectorAll(".btn-tag");
@@ -201,12 +215,12 @@ const btnTag = document.querySelectorAll(".btn-tag");
  * Event listener for tag button that post the tag name to server
  */
 btnTag.forEach(tag => {
-    tag.addEventListener("click", async e => {
-        e.preventDefault();
-        let searchTerm = tag.textContent;
-        await postSearch(searchTerm);
-        console.log(searchTerm);
-    });
+  tag.addEventListener("click", async e => {
+    e.preventDefault();
+    let searchTerm = tag.textContent;
+    await postSearch(searchTerm);
+    console.log(searchTerm);
+  });
 });
 /**
  * postSearch()
@@ -214,37 +228,37 @@ btnTag.forEach(tag => {
  * @param {string} searchTerm - The search term to be sent to the server.
  */
 const postSearch = async searchTerm => {
-    try {
-        let response = await fetch("/search", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ searchTerm })
-        });
-        let responseBody = await response.text();
+  try {
+    let response = await fetch("/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ searchTerm })
+    });
+    let responseBody = await response.text();
 
-        console.log(responseBody);
-        document.body.innerHTML = responseBody;
+    console.log(responseBody);
+    document.body.innerHTML = responseBody;
 
-        if (!response.ok) {
-            throw new Error(`HTTP Error! status: ${response.status}`);
-        }
-    } catch (err) {
-        console.error("Error:", err);
+    if (!response.ok) {
+      throw new Error(`HTTP Error! status: ${response.status}`);
     }
+  } catch (err) {
+    console.error("Error:", err);
+  }
 };
 /**
  * showPassword()
  * Toggle the password type between text and password
  */
 const showPassword = () => {
-    const inputPassword = document.getElementById("password");
-    const confirmPassword = document.getElementById("confirmPassword");
-    const isVisible = inputPassword.type === "text";
+  const inputPassword = document.getElementById("password");
+  const confirmPassword = document.getElementById("confirmPassword");
+  const isVisible = inputPassword.type === "text";
 
-    inputPassword.type = isVisible ? "password" : "text";
-    confirmPassword.type = isVisible ? "password" : "text";
+  inputPassword.type = isVisible ? "password" : "text";
+  confirmPassword.type = isVisible ? "password" : "text";
 };
 // select the show password buttons
 const btnShowPassword = document.querySelectorAll(".btnShowPassword");
@@ -252,14 +266,14 @@ const btnShowPassword = document.querySelectorAll(".btnShowPassword");
  * Event listener for show password button
  */
 btnShowPassword.forEach(btn => {
-    btn.addEventListener("click", e => {
-        e.preventDefault();
-        showPassword();
-    });
+  btn.addEventListener("click", e => {
+    e.preventDefault();
+    showPassword();
+  });
 });
 
 // PLACE ORDER FUNCTION
-const placeOrder = () => {};
+const placeOrder = () => { };
 
 
 /**
@@ -267,21 +281,21 @@ const placeOrder = () => {};
  * Sends search term to the server via post request (IN PROGRESS)
  */
 const search = async (req, res) => {
-    try {
-        const tagElements = document.querySelectorAll(".btn-tag");
-        tagElements.forEach(tag => {
-            const searchTerm = tag.textContent;
-            fetch("/search", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: searchTerm
-            }).then(data => console.log(data));
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const tagElements = document.querySelectorAll(".btn-tag");
+    tagElements.forEach(tag => {
+      const searchTerm = tag.textContent;
+      fetch("/search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: searchTerm
+      }).then(data => console.log(data));
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 
@@ -290,29 +304,82 @@ const btnAddCart = document.querySelectorAll(".btn-add-cart");
 /**
  * Event listener for add to cart buttons
  */
-btnAddCart.forEach(btn => {
-    btn.addEventListener("click", async e => {
-        e.preventDefault();
-        const itemIdElement = btn.querySelector(".item-id");
-        const inputQuantity = btn.querySelector(".input-quantity");
-        
-        const productId = itemIdElement.textContent; // replace with the actual product ID
-        const addCartUrl = "/add-cart/" + productId;
-        try {
-            const response = await fetch(addCartUrl, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                  quantity: inputQuantity.value
-                })
-            });
+// btnAddCart.forEach(btn => {
+//   btn.addEventListener("click", async e => {
+//     e.preventDefault();
+//     const itemIdElement = btn.querySelector(".item-id");
+//     const inputQuantity = btn.querySelector(".input-quantity");
+//     
+//     const color = 
+//     
+//     const productId = itemIdElement.textContent; // replace with the actual product ID
+//     const addCartUrl = "/cart/" + productId + "/add?quantity=" + inputQuantity.value;
+//     try {
+//       const response = await fetch(addCartUrl, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json"
+//         },
+//       });
+// 
+//       const data = await response.json();
+//       console.log(data);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   });
+// });
+// 
 
-            const data = await response.json();
-            console.log(data);
-        } catch (err) {
-            console.error(err);
-        }
-    });
-});
+
+
+const createIssue = async (e, title, description) => {
+  e.preventDefault()
+
+  title = title.value
+  description = description.value
+
+  const url = "https://api.github.com/repos/simplysaad/campus_mart/issues"
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + process.env.GITHUB_PERSONAL_ACCESS_TOKEN
+  }
+  const data = {
+    "title": title,
+    "body": description,
+    "assignees": ["simplysaad"],
+    "labels": ["customer feedback"]
+  }
+
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(data)
+    })
+
+    if (!response.ok) {
+      console.log("error creating new issue")
+    }
+    else {
+      console.log("issue created successfully")
+    }
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
+
+
+const issueForm = document.getElementById("issueForm")
+let title = document.getElementById("title")
+let description = document.getElementById("description")
+let appreciate = document.getElementById("appreciate")
+
+
+appreciate.style.display = "none"
+issueForm.style.display = "block"
+issueForm.addEventListener("submit", createIssue)
+
+
