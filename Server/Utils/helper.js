7c203c30f32bbbee6b9de3013dfd88d11045912c
simplysaad <saadidris70@gmail.com>
2025-03-13 17:10:32 +0100

    modified package.json

--- Server/Utils/helper.js
+++ Server/Utils/helper.js
@@ -13,79 +13,79 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSWORD
+  }
 });
 
 /**
- * Sends an email using a predefined template.
- *
- * @param {string} subject - The subject of the email.
- * @param {string} recipient - The recipient's email address.
- * @param {string} template - The path to the mail template ejs file
- * @param {object} data - The data to be rendered in the email template.
- * @param {string} text - The plain text version of the email.
- *
- * @returns {Promise<void>}
- */
+* Sends an email using a predefined template.
+*
+* @param {string} subject - The subject of the email.
+* @param {string} recipient - The recipient's email address.
+* @param {string} template - The path to the mail template ejs file
+* @param {object} data - The data to be rendered in the email template.
+* @param {string} text - The plain text version of the email.
+*
+* @returns {Promise<void>}
+*/
 const sendMessage = async (subject, recipient, template, data, text) => {
-    try {
-        const htmlContent = await ejs.renderFile(
-            path.resolve(__dirname, template || "../../Test/mail-template.ejs"),
-            data || {}
-        );
-        //let htmlFile = "../../Test/mail-template.html";
-        // let htmlContent = fs.readFileSync(
-        //     path.resolve(__dirname, htmlFile),
-        //     "utf8"
-        // );
-        const mailOptions = {
-            from: process.env.GMAIL_USER,
-            bcc: [recipient],
-            subject,
-            text,
-            html: htmlContent
-        };
+  try {
+    const htmlContent = await ejs.renderFile(
+      path.resolve(__dirname, template || "../../Test/mail-template.ejs"),
+      data || {}
+    );
+    //let htmlFile = "../../Test/mail-template.html";
+    // let htmlContent = fs.readFileSync(
+    //     path.resolve(__dirname, htmlFile),
+    //     "utf8"
+    // );
+    const mailOptions = {
+      from: process.env.GMAIL_USER,
+      bcc: [recipient],
+      subject,
+      text,
+      html: htmlContent
+    };
 
-        const info = await transporter.sendMail(mailOptions);
-        console.log("Email Sent", info);
-        return info;
-    } catch (err) {
-        console.error(err);
-    }
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email Sent", info);
+    return info;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 //helper functions
 const relatedProductsFunc = (arr, n) => {
-    let relatedProducts = new Array();
-    for (let i = 0; i < n; i++) {
-        let randomIndex = Math.floor(Math.random() * arr.length);
+  let relatedProducts = new Array();
+  for (let i = 0; i < n; i++) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
 
-        if (!relatedProducts.includes(arr[randomIndex])) {
-            relatedProducts.push(arr[randomIndex]);
-        }
+    if (!relatedProducts.includes(arr[randomIndex])) {
+      relatedProducts.push(arr[randomIndex]);
     }
-    // console.log(relatedProducts)
-    return relatedProducts;
+  }
+  // console.log(relatedProducts)
+  return relatedProducts;
 };
 
 const readTime = content => {
-    const words = content.split(" ").length;
-    const speed = 200;
+  const words = content.split(" ").length;
+  const speed = 200;
 
-    const readTime = Math.ceil(words / speed) + " min read";
+  const readTime = Math.ceil(words / speed) + " min read";
 
-    return readTime;
+  return readTime;
 };
 
 const findProducts = async query => {
-    let products = await Product.find(query);
-    return products;
+  let products = await Product.find(query);
+  return products;
 };
 // const getCategories = async ()=>{
 //   let categories = await Product.distinct("category")
@@ -102,211 +102,239 @@ const findProducts = async query => {
 //const relatedProducts = relatedProductsFunc(allProducts, 12);
 
 const getCategories = async () => {
-    categoriesPromise = await Product.distinct("category");
-    //console.log("categoriesPromise()", categoriesPromise);
-    return categoriesPromise;
+  categoriesPromise = await Product.distinct("category");
+  //console.log("categoriesPromise()", categoriesPromise);
+  return categoriesPromise;
 };
 
 function destroySession() {
-    req.session.destroy(err => {
-        if (!err) {
-            console.log("session destroyed");
-        }
-    });
-}
-
-async function createIndexes() {
-    try {
-        await Product.createIndexes([
-            {
-                name: "productTextIndex",
-                key: { name: "text", description: "text" }
-            }
-            // ,{ name: "categoryIndex", key: { category: 1 } },
-            // { name: "subCategoryIndex", key: { subCategory: 1 } },
-            // { name: "priceIndex", key: { price: 1 } },
-            // { name: "amountInStockIndex", key: { amountInStock: 1 } },
-            // { name: "tagsIndex", key: { tags: 1 } },
-            // { name: "isFeaturedIndex", key: { isFeatured: 1 } },
-            // { name: "vendorIdIndex", key: { vendorId: 1 } },
-            // {
-            //     name: "variationsIndex",
-            //     key: {
-            //         "variations.size": 1,
-            //         "variations.color": 1,
-            //         "variations.price": 1
-            //     }
-            // }
-        ]);
-        console.log("Indexes created successfully!");
-    } catch (error) {
-        console.error("Error creating indexes:", error);
+  req.session.destroy(err => {
+    if (!err) {
+      console.log("session destroyed");
     }
+  });
 }
 
-/**
- * getFrequencies function
- * takes the array in question as a parameter
- * used to get the frequencies of elements in an Array
- * then create an object for each distinct element
- * then sort the objects by their frequencies
- * then return the sorted array of objects
- */
-
-// const getFrequencies = (arr) => {
-//   //let totalArray = arr.flat()
-//   let freqMap = {}
-//   arr.forEach(element => {
-//     if (freqMap[element]) {
-//       freqMap[element]++
+// async function createIndexes() {
+//   try {
+//     await Product.createIndexes([{
+//       name: "productTextIndex",
+//       key: {
+//         name: "text",
+//         description: "text"
+//       }
 //     }
-//     else {
-//       freqMap[element] = 1
+//       // ,{ name: "categoryIndex", key: { category: 1 } },
+//       // { name: "subCategoryIndex", key: { subCategory: 1 } },
+//       // { name: "priceIndex", key: { price: 1 } },
+//       // { name: "amountInStockIndex", key: { amountInStock: 1 } },
+//       // { name: "tagsIndex", key: { tags: 1 } },
+//       // { name: "isFeaturedIndex", key: { isFeatured: 1 } },
+//       // { name: "vendorIdIndex", key: { vendorId: 1 } },
+//       // {
+//       //     name: "variationsIndex",
+//       //     key: {
+//       //         "variations.size": 1,
+//       //         "variations.color": 1,
+//       //         "variations.price": 1
+//       //     }
+//       // }]);
+//     // console.log("Indexes created successfully!");
+//     } catch (error) {
+//       console.error("Error creating indexes:",
+//         error);
 //     }
-//   })
+//     }
 
-//   let frequencyTable = Object.keys(freqMap).map(key =>
-//   ({
-//     element: key,
-//     frequency: freqMap[key]
-//   })
-//   )
-//   let sortedArray = frequencyTable.sort((a, b) => b.frequency - a.frequency)
+    /**
+    * getFrequencies function
+    * takes the array in question as a parameter
+    * used to get the frequencies of elements in an Array
+    * then create an object for each distinct element
+    * then sort the objects by their frequencies
+    * then return the sorted array of objects
+    */
 
-//   //console.log(arr, freqMap, sortedArray)
-//   return sortedArray
-// }
+    // const getFrequencies = (arr) => {
+    //   //let totalArray = arr.flat()
+    //   let freqMap = {}
+    //   arr.forEach(element => {
+    //     if (freqMap[element]) {
+    //       freqMap[element]++
+    //     }
+    //     else {
+    //       freqMap[element] = 1
+    //     }
+    //   })
 
-const getFrequencies = arr => {
-    const freqMap = arr.reduce((acc, element) => {
+    //   let frequencyTable = Object.keys(freqMap).map(key =>
+    //   ({
+    //     element: key,
+    //     frequency: freqMap[key]
+    //   })
+    //   )
+    //   let sortedArray = frequencyTable.sort((a, b) => b.frequency - a.frequency)
+
+    //   //console.log(arr, freqMap, sortedArray)
+    //   return sortedArray
+    // }
+
+    const getFrequencies = arr => {
+      const freqMap = arr.reduce((acc, element) => {
         acc[element] = (acc[element] || 0) + 1;
         return acc;
-    }, {});
+      },
+        {});
 
-    const frequencyTable = Object.entries(freqMap).map(
+      const frequencyTable = Object.entries(freqMap).map(
         ([element, frequency]) => ({
-            element,
-            frequency
+          element,
+          frequency
         })
-    );
+      );
 
-    // Sort the frequency table by frequency in descending order
-    return frequencyTable.sort((a, b) => b.frequency - a.frequency);
-};
+      // Sort the frequency table by frequency in descending order
+      return frequencyTable.sort((a, b) => b.frequency - a.frequency);
+    };
 
-const sortArray = async searches => {
-    // Flatten the array of search results
-    const flatArray = searches.flatMap(item => item.searchResults);
+    const sortArray = async searches => {
+      // Flatten the array of search results
+      const flatArray = searches.flatMap(item => item.searchResults);
 
-    // Get frequencies of each element
-    const searchProducts = getFrequencies(flatArray);
+      // Get frequencies of each element
+      const searchProducts = getFrequencies(flatArray);
 
-    // Extract only the product objects (excluding frequencies)
-    const sortedProductsArray = searchProducts.map(element => element.element);
-    return sortedProductsArray;
-};
+      // Extract only the product objects (excluding frequencies)
+      const sortedProductsArray = searchProducts.map(element => element.element);
+      return sortedProductsArray;
+    };
 
-const getAge = timestamp => {
-    const units = [
+    const getAge = timestamp => {
+      const units = [{
+        name: "year",
+        factor: 365 * 24 * 60 * 60 * 1000
+      },
         {
-            name: "year",
-            factor: 365 * 24 * 60 * 60 * 1000
+          name: "month",
+          factor: 30 * 24 * 60 * 60 * 1000
         },
         {
-            name: "month",
-            factor: 30 * 24 * 60 * 60 * 1000
+          name: "week",
+          factor: 7 * 24 * 60 * 60 * 1000
         },
         {
-            name: "week",
-            factor: 7 * 24 * 60 * 60 * 1000
+          name: "day",
+          factor: 24 * 60 * 60 * 1000
         },
         {
-            name: "day",
-            factor: 24 * 60 * 60 * 1000
+          name: "hour",
+          factor: 60 * 60 * 1000
         },
         {
-            name: "hour",
-            factor: 60 * 60 * 1000
-        },
-        {
-            name: "minute",
-            factor: 60 * 1000
-        }
-    ];
+          name: "minute",
+          factor: 60 * 1000
+        }];
 
-    let interval = Date.now() - timestamp;
+      let interval = Date.now() - timestamp;
 
-    for (const unit of units) {
+      for (const unit of units) {
         let age = Math.floor(interval / unit.factor);
         if (age > 0) {
-            console.log(`${age} ${age > 1 ? unit.name + "s" : unit.name} ago`);
-            break;
+          console.log(`${age} ${age > 1 ? unit.name + "s": unit.name} ago`);
+          break;
         }
-    }
-};
+      }
+    };
 
-const uploadToImgur = async file => {
-    /**
-     *
-     */
-    try {
+    const cloudinary = require("cloudinary")
+    const uploadToCloudinary = async ()=> {
+      cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        secure: true
+      })
+      try {
+        const response = await cloudinary.uploader.upload(file)
+        return response
+      }catch(err) {
+        console.error(err.message)
+      }
+    }
+
+    const uploadToImgur = async file => {
+      /**
+      *
+      */
+      try {
         //  console.log(file)
         let url = "https://api.imgur.com/3/upload";
         let formData = new FormData();
         let newBlob = new Blob([file.data]);
-        formData.append("image", new Blob([file.data]), file.name);
+        formData.append("image",
+          new Blob([file.data]),
+          file.name);
 
-        let response = await fetch(url, {
+        let response = await fetch(url,
+          {
             method: "POST",
             headers: {
-                Authorization: "Client-ID e2b328ad29f2fa8",
-                "Content-Type": "application/octet-stream"
+              Authorization: "Client-ID e2b328ad29f2fa8",
+              "Content-Type": "application/octet-stream"
             },
             body: formData
-        });
+          });
         let responseJson = await response.json();
-        console.log("responseJson", responseJson);
-        let { link, title, description } = responseJson.data;
+        console.log("responseJson",
+          responseJson);
+        let {
+          link,
+          title,
+          description
+        } = responseJson.data;
 
-        return { link, title, description };
-    } catch (err) {
+        return {
+          link,
+          title,
+          description
+        };
+      } catch (err) {
         console.error(err);
-    }
-};
+      }
+    };
+    // const uploadToImgur = async (files) => {
+    //   const imgurUploads = [];
+    //   for (const file of files) {
+    //     try {
+    //       let url = "https://api.imgur.com/3/upload";
+    //       let formData = new FormData();
+    //       formData.append("image", new Blob([file.data]), file.name);
+    //       let response = await fetch(url, {
+    //         method: "POST",
+    //         headers: {
+    //           Authorization: "Client-ID e2b328ad29f2fa8"
+    //         },
+    //         body: formData
+    //       });
+    //       let responseJson = await response.json();
+    //       console.log("responseJson", responseJson)
+    //       let { link, title, description } = responseJson.data;
+    //       imgurUploads.push({ link, title, description });
+    //     } catch (err) {
+    //       console.error(err);
+    //     }
+    //   }
+    //   return imgurUploads;
+    // }
+    //
 
-// const uploadToImgur = async (files) => {
-//   const imgurUploads = [];
-//   for (const file of files) {
-//     try {
-//       let url = "https://api.imgur.com/3/upload";
-//       let formData = new FormData();
-//       formData.append("image", new Blob([file.data]), file.name);
-//       let response = await fetch(url, {
-//         method: "POST",
-//         headers: {
-//           Authorization: "Client-ID e2b328ad29f2fa8"
-//         },
-//         body: formData
-//       });
-//       let responseJson = await response.json();
-//       console.log("responseJson", responseJson)
-//       let { link, title, description } = responseJson.data;
-//       imgurUploads.push({ link, title, description });
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-//   return imgurUploads;
-// }
-//
-
-module.exports = {
-    relatedProductsFunc,
-    readTime,
-    destroySession,
-    getFrequencies,
-    sortArray,
-    getAge,
-    uploadToImgur,
-    sendMessage
-};
+    module.exports = {
+      relatedProductsFunc,
+      readTime,
+      destroySession,
+      getFrequencies,
+      sortArray,
+      getAge,
+      uploadToImgur,
+      sendMessage
+    };
